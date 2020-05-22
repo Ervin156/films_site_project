d9b34714f1c4763299005f2bd39751c864dbdbbc
Ervin156 <dmitriylystov@gmail.com>
2020-05-22 18:10:47 +0300

    Updates

---
+++ precache-manifest.cfe9e0dfef76f43b28d7676d33e75d91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe99437f787c3cea0fc748aa34330dd",
+    "url": "/films_site_project/index.html"
+  },
+  {
+    "revision": "71c8a5659846cd76fbad",
+    "url": "/films_site_project/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "524bdda88175a64e5745",
+    "url": "/films_site_project/static/css/main.b77a5a26.chunk.css"
+  },
+  {
+    "revision": "71c8a5659846cd76fbad",
+    "url": "/films_site_project/static/js/2.5a6063c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/films_site_project/static/js/2.5a6063c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524bdda88175a64e5745",
+    "url": "/films_site_project/static/js/main.0c97627a.chunk.js"
+  },
+  {
+    "revision": "d1c4e2c768f35963af9a",
+    "url": "/films_site_project/static/js/runtime-main.7339598a.js"
+  }
+]);
